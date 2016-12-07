d786c88ef269e6ace0acf5218f99b98b5d90e986
Lucas <lucas@mindtribe.com>
2016-12-06 20:44:19 -0800

    EEPROM loading working

--- arduinoSantaHat/arduinoSantaHat.ino
+++ arduinoSantaHat/arduinoSantaHat.ino
@@ -30,8 +30,6 @@ enum animation_state_t {
   ANIMATION_BOUNCING,
   // Circling the hat and fading in and out
   ANIMATION_CIRCLING_AND_FADING,
-
-  ANIMATION_MAX
 };
 
 /*
@@ -40,14 +38,67 @@ enum animation_state_t {
 enum speed_state_t {
   SPEED_SLOW = 0,
   SPEED_MEDIUM,
-  SPEED_FAST,
-
-  SPEED_COUNT
+  SPEED_FAST
 };
 
 CRGB leds[LED_STRIP_LENGTH];
 enum animation_state_t animation;
 enum speed_state_t speed_state;
+
+/*
+ * Set the current speed state based on the last one loaded
+ * from EEPROM.
+ */
+enum speed_state_t get_speed_state(enum speed_state_t last_speed_state)
+{
+    enum speed_state_t new_speed_state;
+    switch(last_speed_state) 
+    {
+        case SPEED_SLOW:
+            new_speed_state = SPEED_MEDIUM;
+            break;
+        case SPEED_MEDIUM:
+            new_speed_state = SPEED_FAST;
+            break;
+        case SPEED_FAST:
+            new_speed_state = SPEED_SLOW;
+            break;
+        default:
+          while(1) {
+              Serial.println("== Error setting speed state ==");
+          }
+    }
+    return new_speed_state;
+}
+
+/*
+ * Set the current animation state based on the last one loaded
+ * from EEPROM.
+ */
+enum animation_state_t new_animation_state(enum animation_state_t last_animation_state)
+{
+    enum animation_state_t new_animation_state;
+    switch(last_animation_state) 
+    {
+        case ANIMATION_CIRCLING:
+            new_animation_state = ANIMATION_FADING;
+            break;
+        case ANIMATION_FADING:
+            new_animation_state = ANIMATION_BOUNCING;
+            break;
+        case ANIMATION_BOUNCING:
+            new_animation_state = ANIMATION_CIRCLING_AND_FADING;
+            break;
+        case ANIMATION_CIRCLING_AND_FADING:
+            new_animation_state = ANIMATION_CIRCLING;
+            break;
+        default:
+            while(1) {
+                Serial.println("== Error setting animation");
+            }
+    }
+    return new_animation_state;
+}
 
 void setup() {
   Serial.begin(SERIAL_BAUD_RATE);
@@ -56,13 +107,19 @@ void setup() {
 
   // load state and speed from EEPROM
   enum animation_state_t last_animation_state = (enum animation_state_t)EEPROM.read(EEPROM_ANIMATION_STATE_ADDR);
-  enum speed_state_t last_speed_state =  (enum speed_state_t)(EEPROM.read(EEPROM_SPEED_STATE_ADDR) + 1);
+  enum speed_state_t last_speed_state =  (enum speed_state_t)(EEPROM.read(EEPROM_SPEED_STATE_ADDR));
 
-  EEPROM.write(EEPROM_ANIMATION_STATE_ADDR, last_animation_state);
-  EEPROM.write(EEPROM_SPEED_STATE_ADDR, last_speed_state);
+  speed_state = get_speed_state(last_speed_state);
 
-  animation = last_animation_state;
-  speed_state = last_speed_state;
+  if ((last_speed_state == SPEED_FAST) && (speed_state == SPEED_SLOW)) {
+      animation = new_animation_state(last_animation_state);
+  } else {
+      animation = last_animation_state;
+  }
+  
+  // Set animation and speed
+  EEPROM.write(EEPROM_ANIMATION_STATE_ADDR, animation);
+  EEPROM.write(EEPROM_SPEED_STATE_ADDR, speed_state);
 
   Serial.println(animation);
   Serial.println(speed_state);
