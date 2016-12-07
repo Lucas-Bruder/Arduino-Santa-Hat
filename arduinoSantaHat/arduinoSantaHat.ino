cc4dbc1b275a71dd12d91afe7a96d6ffdbcd418f
Lucas <lucas@mindtribe.com>
2016-12-06 22:05:30 -0800

    Refactoring

--- arduinoSantaHat/arduinoSantaHat.ino
+++ arduinoSantaHat/arduinoSantaHat.ino
@@ -9,7 +9,7 @@
 
 #define SERIAL_BAUD_RATE (115200)
 
-#define LED_DEFAULT_BRIGHTNESS (20U)
+#define LED_DEFAULT_BRIGHTNESS (187)
 
 #define EEPROM_ANIMATION_STATE_ADDR (0)
 #define EEPROM_SPEED_STATE_ADDR     (10)
@@ -20,7 +20,7 @@
 /*
  * Different animations the santa hat will show
  */
-enum animation_state_t {
+enum animation_states_t {
   // Red and green lights constantly circle around the hat
   ANIMATION_CIRCLING = 0,
   // Red and green lights stay stationary, fade in and out
@@ -30,6 +30,8 @@ enum animation_state_t {
   ANIMATION_BOUNCING,
   // Circling the hat and fading in and out
   ANIMATION_CIRCLING_AND_FADING,
+
+  ANIMATION_COUNT,
 };
 
 /*
@@ -38,18 +40,58 @@ enum animation_state_t {
 enum speed_state_t {
   SPEED_SLOW = 0,
   SPEED_MEDIUM,
-  SPEED_FAST
+  SPEED_FAST,
+
+  SPEED_COUNT
 };
 
+typedef void (*animation)(void);
+
+void animate_circling(void);
+
+struct animation_data_t {
+    animation animate;
+    uint16_t delay_ms[SPEED_COUNT];
+
+    // Other data that can be passed around
+    bool even_index_red;
+    bool brightness_increasing;
+    uint8_t led_brightness;
+    uint8_t brightness_jump;
+    
+};
+
+static struct animation_data_t animation_data[ANIMATION_COUNT];// = {
+  //[ANIMATION_CIRCLING] = {.animate = animate_circling, .delay_ms = {300,200,100}, .even_index_red = true, .led_brightness = LED_DEFAULT_BRIGHTNESS},
+//  [ANIMATION_FADING] = {0},
+//  [ANIMATION_BOUNCING] = {0},
+//  [ANIMATION_CIRCLING_AND_FADING] = {0},
+//};
+
 CRGB leds[LED_STRIP_LENGTH];
-enum animation_state_t animation;
-enum speed_state_t speed_state;
-
-// If red index is even, led_index % 2 = red
-bool even_index_red = true;
-
+const int8_t LED_BRIGHTNESS_JUMP = 10;
 uint8_t led_brightness = LED_DEFAULT_BRIGHTNESS;
 bool brightness_increasing = true;
+
+void animate_circling(void)
+{
+    for (uint8_t led_index = 0; led_index < LED_STRIP_LENGTH; led_index++) {
+//        if (even_index_red) {
+//            if (led_index % 2 == 0) {
+//                leds[led_index] = CRGB::Red;
+//            } else {
+//                leds[led_index] = CRGB::Green;
+//            }
+//        } else {
+//            if (led_index % 2 == 0) {
+//                leds[led_index] = CRGB::Green;
+//            } else {
+//                leds[led_index] = CRGB::Red;
+//            }
+//        }
+
+    }
+}
 
 /*
  * Set the current speed state based on the last one loaded
@@ -81,9 +123,9 @@ enum speed_state_t get_speed_state(enum speed_state_t last_speed_state)
  * Set the current animation state based on the last one loaded
  * from EEPROM.
  */
-enum animation_state_t new_animation_state(enum animation_state_t last_animation_state)
+enum animation_states_t new_animation_state(enum animation_states_t last_animation_state)
 {
-    enum animation_state_t new_animation_state;
+    enum animation_states_t new_animation_state;
     switch(last_animation_state) 
     {
         case ANIMATION_CIRCLING:
@@ -106,6 +148,7 @@ enum animation_state_t new_animation_state(enum animation_state_t last_animation
     return new_animation_state;
 }
 
+
 void setup() {
   Serial.begin(SERIAL_BAUD_RATE);
   Serial.println("Starting santa hat");
@@ -113,92 +156,25 @@ void setup() {
   FastLED.setBrightness(led_brightness);
 
   // load state and speed from EEPROM
-  enum animation_state_t last_animation_state = (enum animation_state_t)EEPROM.read(EEPROM_ANIMATION_STATE_ADDR);
+  enum animation_states_t last_animation_state = (enum animation_states_t)EEPROM.read(EEPROM_ANIMATION_STATE_ADDR);
   enum speed_state_t last_speed_state =  (enum speed_state_t)(EEPROM.read(EEPROM_SPEED_STATE_ADDR));
 
-  speed_state = get_speed_state(last_speed_state);
+  enum speed_state_t speed_state = get_speed_state(last_speed_state);
 
+  enum animation_states_t new_animation;
   if ((last_speed_state == SPEED_FAST) && (speed_state == SPEED_SLOW)) {
-      animation = new_animation_state(last_animation_state);
+      new_animation = new_animation_state(last_animation_state);
   } else {
-      animation = last_animation_state;
+      new_animation = last_animation_state;
   }
   
-  EEPROM.write(EEPROM_ANIMATION_STATE_ADDR, animation);
+  EEPROM.write(EEPROM_ANIMATION_STATE_ADDR, new_animation);
   EEPROM.write(EEPROM_SPEED_STATE_ADDR, speed_state);
-}
 
-void show_animation_circling(void)
-{
-    for (uint8_t led_index = 0; led_index < LED_STRIP_LENGTH; led_index++) {
-        if (even_index_red) {
-            if (led_index % 2 == 0) {
-                leds[led_index] = CRGB::Red;
-            } else {
-                leds[led_index] = CRGB::Green;
-            }
-        } else {
-            if (led_index % 2 == 0) {
-                leds[led_index] = CRGB::Green;
-            } else {
-                leds[led_index] = CRGB::Red;
-            }
-        }
-
-    }
-    even_index_red = !even_index_red;
-}
-
-void show_animation_fading(void)
-{
-    uint8_t new_brightness = (brightness_increasing) ? ++led_brightness : --led_brightness;
-    for (uint8_t led_index = 0; led_index < LED_STRIP_LENGTH; led_index++) {
-        if(led_index % 2 == 0) {
-            leds[led_index] = CRGB::Red;
-        } else {
-            leds[led_index] = CRGB::Green;
-        }
-    } 
-
-    Serial.println(new_brightness);
-    FastLED.setBrightness(new_brightness);
-    
-    if (new_brightness == UINT8_MAX || new_brightness == 0) {
-      brightness_increasing = !brightness_increasing;
-    }
+  Serial.println(new_animation);
+  Serial.println(speed_state);
 }
 
 void loop() {
-     show_animation_fading();
-//     switch(animation) {
-//        case ANIMATION_CIRCLING:
-////            show_animation_circling();
-//            break;
-//        case ANIMATION_FADING:
-////            show_animation_fading();
-//            break;
-//        case ANIMATION_BOUNCING:
-//
-//            break;
-//        case ANIMATION_CIRCLING_AND_FADING:
-//
-//            break;
-//    }
-    
-    FastLED.show(); 
-  
-    switch(speed_state) {
-        case SPEED_SLOW:
-            FastLED.delay(500);
-            break;
-        case SPEED_MEDIUM:
-            FastLED.delay(250);
-            break;
-        case SPEED_FAST:
-            FastLED.delay(100);
-            break;
-        default:
-            FastLED.delay(50000);
-            break;
-    }
+
 }
